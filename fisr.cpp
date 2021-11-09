8442a458365b150dd7302cd7e86ff69815c50c67
Claire Yaroslav Yurev <61437734+ClaireYurev@users.noreply.github.com>
2021-11-09 09:39:02 -0800

    Add files via upload

---
+++ fisr.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+float produceFastInverseSquareRoot( float n ) {
+   
+   const float threehalves = 1.5F;
+   float y = n;
+   
+   long i = * ( long * ) &y;
+
+   i = 0x5f3759df - ( i >> 1 );
+   y = * ( float * ) &i;
+   
+   y = y * ( threehalves - ( (n * 0.5F) * y * y ) );
+   
+   return y;
+}
+
+int main(){
+   
+   int n = 512;
+   float fastInverseSquareRoot = produceFastInverseSquareRoot(n);
+   cout<<"The inverse square root of the number "<<n<<" is "<<fastInverseSquareRoot;
+   
+   return 0;
+}
